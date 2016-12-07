a198e8cba1ec31fdf4df95b86fc693870fde69b6
sDromacque <sdromacquedev@gmail.com>
2016-12-07 10:12:43 +0100

    Remove useless code
    remove test connect to api

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,14 +8,7 @@
  * Controller of the frontApp
  */
 angular.module('frontApp')
-  .controller('MainCtrl', function ($http) {
-
-    $http.get('http://localhost:1337/user?skip=5&limit=2')
-      .then(function (res) {
-        var users = res.data;
-        console.log(users);
-      });
-
+  .controller('MainCtrl', function () {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',

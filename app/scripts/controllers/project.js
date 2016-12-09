808e8a7713d3736babc07d195e7f66ad74be60cc
sDromacque <sdromacquedev@gmail.com>
2016-12-09 12:16:10 +0100

    Integrate projectCtrl & create method getProjectByTag

---
+++ app/scripts/controllers/project.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontApp.controller:ProjectCtrl
+ * @description
+ * # ProjectCtrl
+ * Controller of the frontApp
+ */
+angular.module('frontApp')
+  .controller('ProjectByTagCtrl', function ($scope, $http, $routeParams) {
+    let tag = $routeParams.tag;
+    $http.get('http://localhost:1337/project?tag='+tag)
+      .then(function (res) {
+        $scope.projectByTag = res.data;
+      })
+  });
